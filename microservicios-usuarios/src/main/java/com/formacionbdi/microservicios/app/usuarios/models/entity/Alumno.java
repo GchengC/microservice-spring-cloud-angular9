5575db40b3d4794b306df98f32f7b1159a1f071b
GchengC <gchengc12@gmail.com>
2020-11-01 00:21:54 -0400

    Course - Microservicios con Spring Cloud y Angular 9 Section: 1 Video: 8 Description: Creacion de clase entity Alumnos

---
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/models/entity/Alumno.java
@@ -0,0 +1,34 @@
+package com.formacionbdi.microservicios.app.usuarios.models.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author GchengC.
+ * @version v-a.0.0.0.0
+ * @IDE IntelliJ IDEA
+ * @since nov. 2020.
+ **/
+@Entity
+@Table(name = "alumnos")
+@Data
+public class Alumno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    private Date createAt;
+
+    @PrePersist
+    public void prepersist() {
+        this.createAt = new Date();
+    }
+}
