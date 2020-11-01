90586a37c8a161b8b1cf18431bf56928a4f15a40
GchengC <gchengc12@gmail.com>
2020-11-01 01:03:46 -0400

    Course - Microservicios con Spring Cloud y Angular 9 Section: 2 Video: 12 Description: Creacion resto de CRUD en Controller alumno

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -3,10 +3,9 @@ package com.formacionbdi.microservicios.app.usuarios.controllers;
 import com.formacionbdi.microservicios.app.usuarios.models.entity.Alumno;
 import com.formacionbdi.microservicios.app.usuarios.services.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -35,4 +34,32 @@ public class AlumnoController {
 
         return ResponseEntity.ok(o.get());
     }
+
+    @PostMapping()
+    public ResponseEntity<?> crear(@RequestBody Alumno alumno) {
+        Alumno alumnoDB = service.save(alumno);
+        return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDB);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id) {
+        Optional<Alumno> o = service.findById(id);
+
+        if (!o.isPresent())
+            return ResponseEntity.notFound().build();
+
+        Alumno alumnoDB = o.get();
+        alumnoDB.setNombre(alumno.getNombre());
+        alumnoDB.setApellido(alumno.getApellido());
+        alumnoDB.setEmail(alumno.getEmail());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDB));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
