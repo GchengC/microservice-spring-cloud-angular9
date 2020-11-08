719d4dfbd445bf34a8147d44ccb52141bd58b2ba
GchengC <gchengc12@gmail.com>
2020-11-08 17:45:11 -0400

    Course - Microservicios con Spring Cloud y Angular 9 Section: 9 Video: 53 Description: -Agregando Commons y EntityScan

--- microservicio-respuestas/src/main/java/com/formacionbdi/microservicios/app/respuestas/MicroservicioRespuestasApplication.java
+++ microservicio-respuestas/src/main/java/com/formacionbdi/microservicios/app/respuestas/MicroservicioRespuestasApplication.java
@@ -2,8 +2,12 @@ package com.formacionbdi.microservicios.app.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.microservicios.app.respuestas.models.entity",
+        "com.formacionbdi.microservicios.commons.alumnos.models.entity",
+        "com.formacionbdi.microservicios.commons.examenes.models.entity"})
 public class MicroservicioRespuestasApplication {
 
     public static void main(String[] args) {
